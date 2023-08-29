16572b656b2adca667ff0fba98b113abb6d177c2
pramaraju96 <pramaraju96@gmail.com>
2023-08-29 17:33:07 +0530

    Update build.sh

--- vcpkg/scripts/build.sh
+++ vcpkg/scripts/build.sh
@@ -26,6 +26,6 @@ cp -p "${BUILD_DIR}/compile_commands.json" "${PROJECT_DIR}"
 
 # Run tests
 cd "${BUILD_DIR}"
-GTEST_COLOR=1 ctest --verbose
+GTEST_COLOR=1 ctest -O testlog.xml
 
 echo "Build Finished"
